60ecdf8a8113d9711ffaa684246e60643addb52f
kaiholzer <i13008@hb.dhbw-stuttgart.de>
2014-11-18 20:39:51 +0100

    ...

--- MeinHamster.java
+++ MeinHamster.java
@@ -1,117 +1,120 @@
 /**
- * MeinHamster-Klasse von Kai Holzer und Jan Kleinekort
- * Erweitert die Hamster-Klasse.
+ * MeinHamster-Klasse von Kai Holzer und Jan Kleinekort Erweitert die
+ * Hamster-Klasse.
  */
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;class MeinHamster extends Hamster {
-	Akku akku;
-	FixedSizeStack memory;
-	int[][] scannedTerritorium = new int[5][3];
-	
-    MeinHamster() {
-    	super();
-    	erstelleAkku();
-    	erstelleMemory();
-    }
-    
-	MeinHamster(Hamster hamster) {
-		super(hamster);
-    	erstelleAkku();
-    	erstelleMemory();
-	}
-	
-	MeinHamster(int reihe, int spalte, int blickrichtung, int anzahlKoerner) {
-		super(reihe, spalte, blickrichtung, anzahlKoerner);
-    	erstelleAkku();
-    	erstelleMemory();
-	}
-	
-	MeinHamster(int reihe, int spalte, int blickrichtung, int anzahlKoerner, int farbe) {
-		super(reihe, spalte, blickrichtung, anzahlKoerner, farbe);
-    	erstelleAkku();
-    	erstelleMemory();
-	}
-	
-	void erstelleAkku() {
-		akku = new Akku();
-	}
-		
-	void erstelleMemory() {
-		memory  = new FixedSizeStack(10);
-	}
 
-	/**
-	 * Bewegt den Hamster ein Feld vor und nimmt ein Korn auf, falls eins vorhanden ist.
-	 */
-	void meinVor() {
-		if (akku.hatLadung()) {
-			// Ziehe zunächst ein Feld vor und reduziere den Ladezustand des Akkus
-			super.vor();
-			speichereWeg();
-			scanTerritorium();
-			akku.reduziereLadung();
-		
-			// Sammle ein Korn auf, falls auf diesem Feld vorhanden
-			if (super.kornDa())  {
-				super.nimm();
-			}
-			
-		} else {
-			super.schreib("Akku leer :-(");
-		}
-	}
-	
-	/**
-	 * Führt eine Linksdrehung des Hamsters durch und reduziert die Akkuladung entsprechend.
-	 */
-	void meinLinksUm() {
-		if (akku.hatLadung()) {
-			super.linksUm();
-			akku.reduziereLadung();
-		} else {
-			super.schreib("Akku leer :-(");
-		}
-	}
-	
-	/**
-	 * Führt eine Rechtsdrehung des Hamsters durch und reduziert die Akkuladung entsprechend.
-	 */
-	void meinRechtsUm() {
-		if (akku.hatLadung()) {
-			super.linksUm();
-			super.linksUm();
-			super.linksUm();
-			akku.reduziereLadung();
-		} else {
-			super.schreib("Akku leer :-(");
-		}
-	}
-	
-	void meinDreheUm() {
-		meinRechtsUm();
-		meinRechtsUm();
-	}
-	/**
-	 * Speichert den Weg der gelaufen wurde
-	 */
-	void speichereWeg(){
-        memory.push(super.getBlickrichtung());     
-       
+    Akku akku;
+    FixedSizeStack memory;
+    int[][] scannedTerritorium = new int[5][3];
+
+    MeinHamster() {
+        super();
+        erstelleAkku();
+        erstelleMemory();
     }
+
+    MeinHamster(Hamster hamster) {
+        super(hamster);
+        erstelleAkku();
+        erstelleMemory();
+    }
+
+    MeinHamster(int reihe, int spalte, int blickrichtung, int anzahlKoerner) {
+        super(reihe, spalte, blickrichtung, anzahlKoerner);
+        erstelleAkku();
+        erstelleMemory();
+    }
+
+    MeinHamster(int reihe, int spalte, int blickrichtung, int anzahlKoerner, int farbe) {
+        super(reihe, spalte, blickrichtung, anzahlKoerner, farbe);
+        erstelleAkku();
+        erstelleMemory();
+    }
+
+    void erstelleAkku() {
+        akku = new Akku();
+    }
+
+    void erstelleMemory() {
+        memory = new FixedSizeStack(10);
+    }
+
     /**
-	 * Läuft einen Schritt der gespeichert wurde zurück
-	 */
-	void schrittZurueck(){
+     * Bewegt den Hamster ein Feld vor und nimmt ein Korn auf, falls eins
+     * vorhanden ist.
+     */
+    void meinVor() {
+        if (akku.hatLadung()) {
+            // Ziehe zunächst ein Feld vor und reduziere den Ladezustand des Akkus
+            super.vor();
+            akku.reduziereLadung();
+
+            // Sammle ein Korn auf, falls auf diesem Feld vorhanden
+            if (super.kornDa()) {
+                super.nimm();
+            }
+
+        } else {
+            super.schreib("Akku leer :-(");
+        }
+    }
+
+    /**
+     * Führt eine Linksdrehung des Hamsters durch und reduziert die Akkuladung
+     * entsprechend.
+     */
+    void meinLinksUm() {
+        if (akku.hatLadung()) {
+            super.linksUm();
+            akku.reduziereLadung();
+        } else {
+            super.schreib("Akku leer :-(");
+        }
+    }
+
+    /**
+     * Führt eine Rechtsdrehung des Hamsters durch und reduziert die Akkuladung
+     * entsprechend.
+     */
+    void meinRechtsUm() {
+        if (akku.hatLadung()) {
+            super.linksUm();
+            super.linksUm();
+            super.linksUm();
+            akku.reduziereLadung();
+        } else {
+            super.schreib("Akku leer :-(");
+        }
+    }
+
+    void meinDreheUm() {
+        meinRechtsUm();
+        meinRechtsUm();
+    }
+
+    /**
+     * Speichert den Weg der gelaufen wurde
+     */
+    void speichereWeg() {
+        memory.push(super.getBlickrichtung(),super.getReihe(),super.getSpalte());
+
+    }
+
+    /**
+     * Läuft einen Schritt der gespeichert wurde zurück
+     */
+    void schrittZurueck() {
         int richtungLetzterSchritt = memory.pop();
         int blickrichtung = super.getBlickrichtung();
-        
+
         //gegenüberliegende Richtung ermitteln
-        if (richtungLetzterSchritt >= 2){
-            richtungLetzterSchritt= richtungLetzterSchritt-2;
+        if (richtungLetzterSchritt >= 2) {
+            richtungLetzterSchritt = richtungLetzterSchritt - 2;
+        } else {
+            richtungLetzterSchritt = richtungLetzterSchritt + 2;
         }
-        else {
-            richtungLetzterSchritt= richtungLetzterSchritt+2;
-        }
-        
+
         while (richtungLetzterSchritt != blickrichtung) {
             meinLinksUm();
             blickrichtung = super.getBlickrichtung();
@@ -120,7 +123,8 @@ import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Te
         vor();
         akku.reduziereLadung();
     }
-     /**
+
+    /**
      * Scannt die Umgebung vom aktuellen Standpunkt aus, überprüft ob eine Mauer
      * vorhanden ist und legt die ermittelten Werte in einem zweidimensionalen
      * Array ab. -1 --> Keine Mauer auf dieser Koordinate 0 --> Keine Aussage
@@ -129,61 +133,11 @@ import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Te
      *     
 */
     void scanTerritorium() {
-        int blickrichtung = super.getBlickrichtung();
-        int[][] umgebung = new int[16][2];
-        int hamsterPosX = super.getSpalte();
-        int hamsterPosY = super.getReihe();
-        int speicherPos =0;
-        
+
         // Den Array mit 0 initialisieren bzw. resetten
         initialiereScannedTerritorium();
-        switch (blickrichtung) {
-            //Nord
-            case 0:
-            	speicherPos=0;
-                for (int y = -2; y <= 0; y++) {
-                    for (int x = -2; x <= 2; x++) {
-                        speicherPos++;
-                        umgebung[speicherPos][0] = hamsterPosX + x;
-                        umgebung[speicherPos][1] = hamsterPosY + y;
-                    }
-                }
-                break;
-            //Ost
-            case 1:
-            	speicherPos=0;
-                for (int x = +2; x >= 0; x--) {
-                    for (int y = -2; y <= 2; y++) {
-                        speicherPos++;
-                        umgebung[speicherPos][0] = hamsterPosX + x;
-                        umgebung[speicherPos][1] = hamsterPosY + y;
-                    }
-                }
-                break;
-            //Süd
-            case 2:
-            	speicherPos=0;
-                for (int y = +2; y >= 0; y--) {
-                    for (int x = +2; x >= -2; x--) {
-                        speicherPos++;
-                        umgebung[speicherPos][0] = hamsterPosX + x;
-                        umgebung[speicherPos][1] = hamsterPosY + y;
+        int[][] umgebung = getPositions();
 
-                    }
-                }
-                break;
-            //West
-            case 3:
-            	speicherPos=0;
-                for (int x = -2; x <= 0; x++) {
-                    for (int y = +2; y >= -2; y--) {
-                    	speicherPos++;
-                        umgebung[speicherPos][0] = hamsterPosX + x;
-                        umgebung[speicherPos][1] = hamsterPosY + y;
-                    }
-                }
-                break;
-        }
         // 0 0 0 0 0
         // 0 0 0 0 0
         // 0 X H 0 0 
@@ -356,5 +310,95 @@ import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Te
         }
 
     }
-    
+
+    int[][] getPositions() {
+        int blickrichtung = super.getBlickrichtung();
+        int[][] umgebung = new int[16][2];
+        int hamsterPosSpalte = super.getSpalte();
+        int hamsterPosReihe = super.getReihe();
+
+        int speicherPos = 0;
+        //speicherPos Anordnung
+        // 1  2  3  4  5
+        // 6  7  8  9  10
+        // 11 12 13 14 15
+        switch (blickrichtung) {
+            //Nord
+            case 0:
+                speicherPos = 0;
+                for (int y = -2; y <= 0; y++) {
+                    for (int x = -2; x <= 2; x++) {
+                        speicherPos++;
+                        umgebung[speicherPos][1] = hamsterPosSpalte + x;
+                        umgebung[speicherPos][0] = hamsterPosReihe + y;
+                    }
+                }
+                break;
+            //Ost
+            case 1:
+                speicherPos = 0;
+                for (int x = +2; x >= 0; x--) {
+                    for (int y = -2; y <= 2; y++) {
+                        speicherPos++;
+                        umgebung[speicherPos][1] = hamsterPosSpalte + x;
+                        umgebung[speicherPos][0] = hamsterPosReihe + y;
+                    }
+                }
+                break;
+            //Süd
+            case 2:
+                speicherPos = 0;
+                for (int y = +2; y >= 0; y--) {
+                    for (int x = +2; x >= -2; x--) {
+                        speicherPos++;
+                        umgebung[speicherPos][1] = hamsterPosSpalte + x;
+                        umgebung[speicherPos][0] = hamsterPosReihe + y;
+
+                    }
+                }
+                break;
+            //West
+            case 3:
+                speicherPos = 0;
+                for (int x = -2; x <= 0; x++) {
+                    for (int y = +2; y >= -2; y--) {
+                        speicherPos++;
+                        umgebung[speicherPos][1] = hamsterPosSpalte + x;
+                        umgebung[speicherPos][0] = hamsterPosReihe + y;
+                    }
+                }
+                break;
+        }
+        return umgebung;
+    }
+
+    void umgebungAnalysieren() {
+        scanTerritorium();
+        pruefeUmgebung();
+    }
+
+    void pruefeUmgebung() {
+        if (scannedTerritorium[1][2] == 1 && scannedTerritorium[2][1] == 1) {
+            if (scannedTerritorium[3][2] == -1) {
+                meinRechtsUm();
+            } else {
+                schrittZurueck();
+            }
+        }
+        if (!memory.pruefeKoerner()){
+        	meinRechtsUm();
+        	memory.initKoernerArray();
+        }
+        
+    }
+
+    void reinige() {
+        while (akku.hatLadung()) {
+            umgebungAnalysieren();
+            meinVor();
+            speichereWeg();
+
+        }
+    }
+
 }
